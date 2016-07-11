3fe53d76d8f1c00ad1939fe75824d498d8058a85
Will Charczuk <will@blendlabs.com>
2016-07-10 23:27:38 -0700

    tick test.

--- tick_test.go
+++ tick_test.go
@@ -1,1 +1,14 @@
 package chart
+
+import (
+	"testing"
+
+	"github.com/blendlabs/go-assert"
+)
+
+func TestGenerateTicksWithStep(t *testing.T) {
+	assert := assert.New(t)
+
+	ticks := GenerateTicksWithStep(Range{Min: 1.0, Max: 10.0, Domain: 100}, 1.0, FloatValueFormatter)
+	assert.Len(ticks, 9)
+}
