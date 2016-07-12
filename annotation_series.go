52e80c183dc6093b475d9b6568302af7c6785b7a
Will Charczuk <will@clotheshor.se>
2016-07-11 18:51:59 -0700

    laying groundwork for grid lines and a refactor of translate

--- annotation_series.go
+++ annotation_series.go
@@ -49,7 +49,7 @@ func (as AnnotationSeries) Measure(r Renderer, canvasBox Box, xrange, yrange Ran
 			Padding:     DefaultAnnotationPadding,
 		})
 		for _, a := range as.Annotations {
-			lx := canvasBox.Left - xrange.Translate(a.X)
+			lx := canvasBox.Left + xrange.Translate(a.X)
 			ly := canvasBox.Bottom - yrange.Translate(a.Y)
 			ab := MeasureAnnotation(r, canvasBox, style, lx, ly, a.Label)
 			box.Top = MinInt(box.Top, ab.Top)
