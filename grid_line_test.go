296ed0fd3b8c33ccf7ac063a207b910569b47c3c
Will Charczuk <will.charczuk@gmail.com>
2016-07-15 13:43:53 -0700

    gofmt -s

--- grid_line_test.go
+++ grid_line_test.go
@@ -10,10 +10,10 @@ func TestGenerateGridLines(t *testing.T) {
 	assert := assert.New(t)
 
 	ticks := []Tick{
-		Tick{Value: 1.0, Label: "1.0"},
-		Tick{Value: 2.0, Label: "2.0"},
-		Tick{Value: 3.0, Label: "3.0"},
-		Tick{Value: 4.0, Label: "4.0"},
+		{Value: 1.0, Label: "1.0"},
+		{Value: 2.0, Label: "2.0"},
+		{Value: 3.0, Label: "3.0"},
+		{Value: 4.0, Label: "4.0"},
 	}
 
 	gl := GenerateGridLines(ticks, true)
