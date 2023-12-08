18fde3032a85e5ea521594054e454d4037f966c2
duynv <duynvwork@gmail.com>
2023-12-08 22:20:57 +0700

    refactor: Update TimeToFloat64/TimeFromFloat64 functions, cover case time is empty and unittest

--- timeutil.go
+++ timeutil.go
@@ -77,11 +77,17 @@ func TimeMinMax(times ...time.Time) (min, max time.Time) {
 
 // TimeToFloat64 returns a float64 representation of a time.
 func TimeToFloat64(t time.Time) float64 {
+	if t.IsZero() {
+		return 0
+	}
 	return float64(t.UnixNano())
 }
 
-// TimeFromFloat64 returns a time from a float64.
+// TimeFromFloat64 returns a time in nanosecond from a float64.
 func TimeFromFloat64(tf float64) time.Time {
+	if tf == 0 {
+		return time.Time{}
+	}
 	return time.Unix(0, int64(tf))
 }
 
